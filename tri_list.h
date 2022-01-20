3e03517ab56b6dcb49280d2c6777348a0cb3b4ea
barteksad <barteksadlej@gmail.com>
2022-01-20 18:03:48 +0100

    iterators works

--- tri_list.h
+++ tri_list.h
@@ -7,6 +7,8 @@
 #include <list>
 #include <ranges>
 #include <functional>
+#include <iterator>
+#include <cstddef>
 
 namespace
 {
@@ -20,13 +22,48 @@ template <typename T1, typename T2, typename T3>
 class tri_list
 {
 private:
-    using tri_type = std::variant<T1, T2, T3>;
-    using tri_container = std::list<tri_type>;
+    using tri_type_t = std::variant<T1, T2, T3>;
+    using tri_container = std::list<tri_type_t>;
+    using tri_modifiers_f = std::function<tri_type_t(tri_type_t)>;
 
     tri_container elems;
-    std::function<tri_type(tri_type)> modifiers;
+    tri_modifiers_f modifiers;
+
+    struct Iterator 
+    {
+        private:
+
+            typename tri_container::const_iterator m_ptr;
+            tri_modifiers_f m_modifiers;
+
+        public:
+            using iterator_category = std::forward_iterator_tag;
+            using difference_type   = std::ptrdiff_t;
+            using value_type        = tri_type_t;
+            using pointer           = const value_type*;
+            using reference         = const value_type;
+
+            Iterator(decltype(m_ptr) _ptr, tri_modifiers_f _modifiers) : m_ptr(_ptr), m_modifiers(_modifiers) {}
+
+            reference operator*() const 
+            {
+                auto value = m_modifiers(*m_ptr);
+                return value; 
+            }
+
+            pointer operator->() { return m_ptr; }
+
+            Iterator& operator++() { m_ptr++; return *this; }  
+
+            Iterator operator++(int) { Iterator tmp = *this; ++(*this); return tmp; }
+
+            friend bool operator== (const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; };
+            friend bool operator!= (const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; };  
+
+    };
 
 public:
+
     tri_list()
     {
         modifiers = std::identity();
@@ -43,11 +80,11 @@ public:
     requires one_of_tri<T, T1, T2, T3>
     void modify_only(F m = F{})
     {
-        modifiers = [*this, m](tri_type elem) -> tri_type
+        modifiers = [*this, m](tri_type_t elem) -> tri_type_t
         {
             elem = modifiers(elem);
             if (std::holds_alternative<T>(elem))
-                return tri_type(std::invoke(m, std::get<T>(elem)));
+                return tri_type_t(std::invoke(m, std::get<T>(elem)));
             else
                 return elem;
         };
@@ -57,19 +94,25 @@ public:
     requires one_of_tri<T, T1, T2, T3>
     void reset()
     {
-        modifiers = std::identity();
+        modifiers = [*this](tri_type_t elem) -> tri_type_t
+        {
+            if (std::holds_alternative<T>(elem))
+                return elem;
+            else
+                return tri_type_t(std::invoke(modifiers, elem));
+        };
     }
 
     template <typename T>
     requires one_of_tri<T, T1, T2, T3>
     auto range_over()
     {
-        auto filter_modifier = [](const tri_type &elem)
+        auto filter_modifier = [](const tri_type_t &elem)
         {
             return std::holds_alternative<T>(elem);
         };
 
-        auto get_value = [](const tri_type &elem)
+        auto get_value = [](const tri_type_t &elem)
         {
             return std::get<T>(elem);
         };
@@ -78,6 +121,9 @@ public:
                      | std::views::filter(filter_modifier) 
                      | std::views::transform(get_value);
     }
+
+    Iterator begin() { return Iterator(elems.begin(), modifiers); }
+    Iterator end()   { return Iterator(elems.end(), modifiers); }
 };
 
 #endif // TRI_LIST_H
